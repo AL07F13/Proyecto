716fcf06e2f9244d9878c22f3eab1584dd068d21
AL07F13 <ceronherrey@gmail.com>
2015-06-24 11:15:49 -0500

    uno
    aplicacion delfínes

---
+++ js/acciones.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#mypanel').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a mypanel
+	$('#delfines').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a delfines
+	$('#especies').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a delfines
+	$('#alimentacion').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a delfines
+	$('#respiracion').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a delfines
+	$('#reproduccion').on('tap',function (){
+		navigator.vibrate(1000);
+		}); // tap a delfines
+	
+}); 
+});
+
