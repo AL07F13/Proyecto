dd4930c2605df1d6009f34212ab8e436b6ce6f70
AL07F13 <ceronherrey@gmail.com>
2015-06-25 09:23:08 -0500

    cuatro
    delfines

--- js/acciones.js
+++ js/acciones.js
@@ -3,22 +3,22 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$('#mypanel').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a mypanel
+		});
 	$('#delfines').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a delfines
+		});
 	$('#especies').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a delfines
+		});
 	$('#alimentacion').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a delfines
+		});
 	$('#respiracion').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a delfines
+		});
 	$('#reproduccion').on('tap',function (){
 		navigator.vibrate(1000);
-		}); // tap a delfines
+		});
 	
 }); 
 });
